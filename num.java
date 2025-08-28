9e43cecced8cd883c805162212812c0a4767bab0
Hansikasinha <hansikaa.sinha@gmail.com>
2025-08-28 12:10:48 +0530

    Add files via upload

---
+++ num.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class num{
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter two numbers");  
+    int a =sc.nextInt();
+    int b =sc.nextInt();  
+    int c=a+b;
+    System.out.println("addition is"+c);
+    }
+}
