fb94369f7f86ae79ba296e00cd59fbc6034e4fb7
Hansikasinha <hansikaa.sinha@gmail.com>
2025-08-28 12:10:03 +0530

    Add files via upload

---
+++ greet.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class greet{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter name");
+        System.out.println("enter age");
+        System.out.println("hello");
+    }
+}
