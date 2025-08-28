20226d85d404af503a4532e0ac1ea8f43e90c5ff
Hansikasinha <hansikaa.sinha@gmail.com>
2025-08-28 12:11:13 +0530

    Add files via upload

---
+++ sq.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class sq{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        int a=sc.nextInt();
+        int b=a*a;
+        System.out.println("square of a number"+b);
+    }
+}
