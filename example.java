74d9f40c7be20f94b1c43debb60503fc6421a768
Hansikasinha <hansikaa.sinha@gmail.com>
2025-09-17 13:09:43 +0530

    Add files via upload

---
+++ example.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class example{
+    public static void main(String[] args) {
+            Scanner sc= new Scanner (System.in);
+            System.out.println("enter the string");
+            String s1=sc.nextLine();
+            System.out.println(s1.length());
+    }
+}
