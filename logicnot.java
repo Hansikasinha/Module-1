919c66a01286174426d565f50c3617cebe007398
Hansikasinha <hansikaa.sinha@gmail.com>
2025-09-17 13:13:10 +0530

    Add files via upload

---
+++ logicnot.java
@@ -0,0 +1,11 @@
+public class logicnot{
+    public static void main(String[] args) {
+        boolean israining=true;
+        if(!israining){
+            System.out.println("you can go outside without umbrella");
+        }
+        else{
+            System.out.println("take an umbrella");
+        }
+    }
+}
