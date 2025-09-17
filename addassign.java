b62cbda773f28a25d851f999afe2664c2a70ca37
Hansikasinha <hansikaa.sinha@gmail.com>
2025-09-17 13:07:11 +0530

    Add files via upload

---
+++ addassign.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class addassign{
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       System.out.println("enter x");
+       int x=sc.nextInt();
+       x+=5;
+        System.out.println("x="+x);
+    }
+}
