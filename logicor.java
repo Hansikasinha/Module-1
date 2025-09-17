b4eb39e55f7754b597b91dacc544a298eb277484
Hansikasinha <hansikaa.sinha@gmail.com>
2025-09-17 13:13:35 +0530

    Add files via upload

---
+++ logicor.java
@@ -0,0 +1,12 @@
+public class logicor{
+    public static void main(String[] args) {
+        boolean isstudent=false;
+        boolean isteacher=false;
+        if(isstudent|| isteacher){
+            System.out.println("you can enter the campus");
+        }
+        else{
+            System.out.println("access denied");
+        }
+    }
+}
